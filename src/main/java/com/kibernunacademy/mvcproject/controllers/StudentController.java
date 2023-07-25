8e2c8a2ccea4dac2ff108ad05c7904b2e5b2cd69
Miguel Ramos <return.infox@gmail.com>
2023-07-25 19:56:10 -0400

    implementacion mvc

--- src/main/java/com/kibernunacademy/mvcproject/controllers/StudentController.java
+++ src/main/java/com/kibernunacademy/mvcproject/controllers/StudentController.java
@@ -1,4 +1,27 @@
 package com.kibernunacademy.mvcproject.controllers;
 
+import com.kibernunacademy.mvcproject.services.IStudentService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller // RestController
 public class StudentController {
+
+  private final IStudentService service;
+
+  @Autowired
+  public StudentController(IStudentService service) {
+    this.service = service;
+  }
+
+  // http://localhost:8080/students
+  // http://localhost:8080
+  @GetMapping({"/students", "/"})
+  public String getAllStudents(Model model) {
+    model.addAttribute("students", service.listAllStudents());
+    return "index";
+  }
+
 }
