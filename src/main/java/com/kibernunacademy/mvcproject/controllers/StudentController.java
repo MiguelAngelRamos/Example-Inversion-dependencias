1892bb304f2ef771cfc5de3f2596a9bbd6a6ec20
Miguel Ramos <return.infox@gmail.com>
2023-07-27 17:58:06 -0400

    crear el formulario y guardar estudiante

--- src/main/java/com/kibernunacademy/mvcproject/controllers/StudentController.java
+++ src/main/java/com/kibernunacademy/mvcproject/controllers/StudentController.java
@@ -1,10 +1,13 @@
 package com.kibernunacademy.mvcproject.controllers;
 
+import com.kibernunacademy.mvcproject.entitys.Student;
 import com.kibernunacademy.mvcproject.services.IStudentService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller // @RestController (cuando queramos retornar un json)
 public class StudentController {
@@ -22,6 +25,22 @@ public class StudentController {
   public String getAllStudents(Model model) {
     model.addAttribute("students", service.listAllStudents());
     return "index";
+  }
+  // Para llamar al formulario para crear un estudiante
+  // http://localhost:8080/student/new
+  @GetMapping("/student/new")
+  public String createStudentForm(Model model) {
+    Student student = new Student();
+    model.addAttribute("student", student);
+    return "create-student"; // create-student.html
+  }
+
+  // Para guardar el estudiante
+  // http://localhost:8080/student
+  @PostMapping("/student")
+  public String saveStudent(@ModelAttribute("student") Student student) {
+    service.saveStudent(student);
+    return "redirect:/index";
   }
 
 }
