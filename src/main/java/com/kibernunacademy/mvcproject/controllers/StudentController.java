68106f2cf303d98bd3d8c0706b0c208eafd5fd35
Miguel Ramos <return.infox@gmail.com>
2023-07-27 19:52:08 -0400

    actualizar un estudiante

--- src/main/java/com/kibernunacademy/mvcproject/controllers/StudentController.java
+++ src/main/java/com/kibernunacademy/mvcproject/controllers/StudentController.java
@@ -7,7 +7,10 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+
+import java.util.Optional;
 
 @Controller // @RestController (cuando queramos retornar un json)
 public class StudentController {
@@ -40,7 +43,59 @@ public class StudentController {
   @PostMapping("/student")
   public String saveStudent(@ModelAttribute("student") Student student) {
     service.saveStudent(student);
-    return "redirect:/index";
+    return "redirect:/";
   }
 
+  // Un controlador que llame al formulario para editar un estudiante
+  // http://localhost:8080/student/edit/1
+  @GetMapping("/student/edit/{id}")
+  public String showFormEditStudent(@PathVariable Long id, @ModelAttribute("student") Student student, Model model) {
+    Optional<Student> optionalStudent = service.getStudentById(id);
+
+    if(!optionalStudent.isPresent()) {
+      throw new StudentNotFoundException("No se encontro el estudiante con ese id: " +  id);
+    }
+    Student studentExists = optionalStudent.get();
+    model.addAttribute("student", studentExists);
+    return "edit-student";
+
+    /*if(optionalStudent.isPresent()) {
+      Student studentExists = optionalStudent.get();
+      model.addAttribute("student", student);
+      return "edit-student";
+    } else {
+      // generar la excepción
+      throw  new StudentNotFoundException("No se encontro el estudiante con ese id: " +  id);
+    } */
+
+  }
+
+  // Vamos actualizar al Estudiante
+
+  @PostMapping("/student/{id}")
+  public String updateStudent(@PathVariable Long id, @ModelAttribute("student") Student student, Model model) {
+    // comprobar que existe el usuario que queremos actualizar
+    Optional<Student> optionalStudent = service.getStudentById(id);
+
+    if(!optionalStudent.isPresent()) {
+      throw new StudentNotFoundException("No se encontro un estudiante con ese id: " +  id);
+    }
+    Student studentExists = optionalStudent.get(); // obtengo al estudiante por su id (de la base de datos para actualizarlo)
+
+    // Actualizamos al estudiante
+    studentExists.setId(id);
+    studentExists.setName(student.getName());
+    studentExists.setLastname(student.getLastname());
+    studentExists.setEmail(student.getEmail());
+
+    service.updateStudent(studentExists);
+    return "redirect:/";
+
+}
+}
+
+class StudentNotFoundException extends RuntimeException {
+  public  StudentNotFoundException (String message) {
+    super(message);
+  }
 }
