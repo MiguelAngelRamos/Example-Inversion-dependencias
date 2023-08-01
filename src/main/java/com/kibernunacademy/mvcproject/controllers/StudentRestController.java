24c5f93c3f5f3591dbc8cc24ae26596b7ded0b95
Miguel Ramos <return.infox@gmail.com>
2023-08-01 19:03:32 -0400

    end point

--- src/main/java/com/kibernunacademy/mvcproject/controllers/StudentRestController.java
+++ src/main/java/com/kibernunacademy/mvcproject/controllers/StudentRestController.java
@@ -4,12 +4,10 @@ import com.kibernunacademy.mvcproject.entitys.Student;
 import com.kibernunacademy.mvcproject.services.IStudentService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseStatus;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/api")
@@ -19,11 +17,33 @@ public class StudentRestController {
   private IStudentService studentService;
 
   // localhost:8080/api/students
-
   @GetMapping("/students")
   @ResponseStatus(HttpStatus.OK)
   public List<Student> getStudentAll() {
     return studentService.listAllStudents();
   }
+
+  // localhost:8080/api/student/1
+  @GetMapping("/student/{id}")
+  public Optional<Student> getStudentById(@PathVariable Long id) {
+
+    Optional<Student> optionalStudent = studentService.getStudentById(id);
+    if(!optionalStudent.isPresent()) throw new StudentNotFoundException("No existe un Student con ese id: "+ id);
+    return optionalStudent;
+  }
+
+  // crear un estudiante
+  // localhost:8080/api/student
+  @PostMapping("/student")
+  public Student createStudent(@RequestBody Student student) {
+    return studentService.saveStudent(student);
+  }
+
+  // Actualizar el estudiante
+
+
+  // Eliminar el estudiante
+
+
 
 }
